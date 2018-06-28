495a55789218b72f6488eaab7c7ff4ecb232736d
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2018-06-28 13:59:36 +0530

    Clean up for output mgmt

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import  _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
 
     // Dependency: Lodash
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    element.classList.add('hello');
-
-    // Add the image to our existing div
-    var myIcon = new Image();
-    myIcon.src = Icon; // icon.jpg is available in the form of Icon
-    element.appendChild(myIcon);
     
-    console.log(Data)
-
     return element;
 }
 
